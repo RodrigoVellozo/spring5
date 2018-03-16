27a86ec1d837896541574d3e0843796583d72c60
rodri <rodri@192.168.0.12>
2018-03-16 13:50:24 -0300

    Commit inicial.

---
+++ spring1/src/main/java/com/application/model/User.java
@@ -0,0 +1,44 @@
+package com.application.model;
+
+import java.util.Set;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="user")
+public class User {
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	@Column(name="id")
+	private Long id;
+	
+	@Column(name="name")
+	private String name;
+	
+	@ManyToMany
+	private Set<Role> roles;
+
+	//getters and setters
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
